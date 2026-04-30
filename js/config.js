1b6401381db4e82c10d121f4a01dfbac503118e5
Dillip-J <dillipprem@gmail.com>
2026-04-30 13:59:21 +0530

    auto complete address implementation

--- js/config.js
+++ js/config.js
@@ -1,22 +1,22 @@
 // js/config.js
 
-// if (window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost' || window.location.protocol === 'file:') {
-//     // 🚨 EXPLICITLY attach to window so other scripts can see it!
-//     window.API_BASE = 'http://127.0.0.1:8000';
-//     console.log("🔌 Connected to LOCAL Backend");
-// } else {
-//     window.API_BASE = 'https://backend-depolyment-3.onrender.com'; 
-//     console.log("☁️ Connected to LIVE Cloud Backend");
-// }
+if (window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost' || window.location.protocol === 'file:') {
+    // 🚨 EXPLICITLY attach to window so other scripts can see it!
+    window.API_BASE = 'http://127.0.0.1:8000';
+    console.log("🔌 Connected to LOCAL Backend");
+} else {
+    window.API_BASE = 'https://backend-depolyment-3.onrender.com'; 
+    console.log("☁️ Connected to LIVE Cloud Backend");
+}
 // // ==========================================
 // // 🚨 GLOBAL API ROUTER (LOAD THIS FIRST)
 // // ==========================================
-let API_BASE;
+// let API_BASE;
 
-if (window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost' || window.location.protocol === 'file:') {
-    API_BASE = 'http://127.0.0.1:8000';
-    console.log("🔌 Connected to LOCAL Backend");
-} else {
-    API_BASE = 'https://backend-depolyment-3.onrender.com'; 
-    console.log("☁️ Connected to LIVE Cloud Backend");
-}
+// if (window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost' || window.location.protocol === 'file:') {
+//     API_BASE = 'http://127.0.0.1:8000';
+//     console.log("🔌 Connected to LOCAL Backend");
+// } else {
+//     API_BASE = 'https://backend-depolyment-3.onrender.com'; 
+//     console.log("☁️ Connected to LIVE Cloud Backend");
+// }
